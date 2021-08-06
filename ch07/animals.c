c474dc9320cada68923bb79b360a1a356b322638
dchev001 <dchev001@gmail.com>
2021-08-05 21:37:37 -0400

    chapter 7

---
+++ ch07/animals.c
@@ -0,0 +1,63 @@
+/* animals.c -- uses a switch statement */
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+  char ch;
+
+  printf("Give me a letter fo the alphabet, and I will give ");
+  printf("an animal name\nbeginning with that letter.\n");
+  printf("Please type in a letter; type # to end my act.\n");
+
+  while ((ch = getchar()) != '#')
+  {
+    if ('\n' == ch)
+    {
+      continue;
+    }
+
+    if (islower(ch))
+    {
+      switch (ch)
+      {
+        case 'a':
+		printf("argali, a wild sheep of Asia\n");
+		break;
+	case 'b':
+		printf("Babirusa, a wild pig fo Malay\n");
+		break;
+	case 'c':
+		printf("coati, racoonlike mammal\n");
+		break;
+	case 'd':
+		printf("desman, aquatic, molelike critter\n");
+		break;
+	case 'e':
+		printf("echidna, the spiny anteater\n");
+		break;
+	case 'f':
+		printf("fisher, brownish marten\n");
+		break;
+	default:
+		printf("That's a sumper!\n");
+		// end of switch
+      }
+    }
+    else
+    {
+      printf("I recognize only lowercase letters.\n");
+    }
+  
+    while (getchar() != '\n')
+    {    
+      continue;    // skip rest of input line
+    } 
+
+    printf("Please type another letter or a #.\n");
+  }
+
+  printf("Bye!\n");
+
+  return 0;
+}
---
+++ ch07/divisors.c
@@ -0,0 +1,43 @@
+/* divisors.c -- nseted ifs display divisors of a number */
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+  unsigned long num;          // number to be checked
+  unsigned long div;          // potential divisors
+  bool isPrime;               // prime flag
+
+  printf("PLease enter an integer for analysis; ");
+  printf("Enter q to quit.\n");
+
+  while (scanf("%lu", &num) == 1)
+  {
+    for (div = 2, isPrime = true; (div * div) <= num; div++)
+    {
+      if (num % div == 0)
+      {
+        if ((div * div) != num)
+	{
+	  printf("%lu is divisible by %lu and %lu.\n", num, div, num/div);
+	}
+        else
+        {
+          printf("%lu is divisible by %lu.\n", num, div);
+        }
+      }
+    }
+
+    if (isPrime)
+    {
+      printf("%lu is prime.\n", num);
+    }
+
+    printf("Please enter another integer for analysis; ");
+    printf("Enter q to quit.\n");
+  }
+
+  printf("Bye.\n");
+
+  return 0;
+}
